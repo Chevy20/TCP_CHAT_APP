e8f0bbb94914bcb896a3bb8ba9c6cedab0fca05a
Matthew Cheverie <mcheveri@uwo.ca>
2021-11-15 17:28:34 -0500

    Push from laptop

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
-# CS 3357 Assignment 1 Matthew Cheverie 251098050
+# CS 3357 Assignment 3 Matthew Cheverie 251098050
 # This file runs the chat client for the user. It uses select to handle multiplexing. Sys for dealing with the terminal.
-# uses urlparse to parse through the command line arguments. Uses signal to handle control C inputs
+# uses urlparse to parse through the command line arguments. Uses signal to handle control C inputs. Users can follow
+# other users and terms. They can unfollow terms and users as well. Files can also be sent. This works by sending file
+# to server and creating a copy stored on the server. It then gets all the users it needs to send it to and reads data
+# from file stored on server and sends it to all required users.
 from socket import *
 import select
 import sys
 from urllib.parse import urlparse
 import signal
+import os
 
 
 # Function that will handle control c inputs from the server. It will print a message saying that the client will be
@@ -79,7 +83,6 @@ print("Connected to host! Awaiting registration")
 regMes = "REGESTER " + username + " CHAT/1.0"  # create registration command to server
 clientSock.send(regMes.encode())  # send reg command to server
 regUserWithClient(clientSock)  # Get response from server and continue if successful, exit if not
-
 prompt()  # Write the prompt for the terminal, where the user will enter text
 
 socketList = [sys.stdin, clientSock]  # Create the socket list for this client
@@ -95,7 +98,7 @@ while True:
 
         # This will be when a message is sent to the client, either from another user or the server
         if sock == clientSock:
-            data = sock.recv(4096)
+            data = sock.recv(1024)
 
             # If no data is recived and error occurred and the client is disconnected
             if not data:
@@ -104,18 +107,129 @@ while True:
                 sys.exit()
 
             # If a server disconnect message is received, shut down this client and exit the system
-            elif data.decode() == "DISCONNECT CHAT/1.0":
+            if data.decode() == "DISCONNECT CHAT/1.0":
                 print("Server is shutting down! All clients will be disconnected")
                 clientSock.close()
                 sys.exit(0)
 
+            # The message displayed when the server confirms that the !exit command was excetured
+            if data.decode() == "DISCONNECT EXIT":
+                print("Disconnected from server! .... Exiting the program. \n Goodbye!")
+                clientSock.close()
+                sys.exit(0)
+
+            # This will get triggered when the server sends a request for a file.
+            if data.decode().startswith("REQUEST"):
+               # Get the request from the server, split into a list. Fileneame contained in index 1
+                req = data.decode().split(" ")
+                filesend = None  # initialize varible to none, make its scope outside of both if statements so it can be used between varibles
+                count = 0        # set count for bytes read to 0
+               # IF the requested file is a text file
+                if req[1].endswith("txt"):
+                    flag = 1    # set a flag used to see if the file could be opened or not to 1 (default)
+
+                    # Try to open File, if file cannot be opened, set flag to -1
+                    try:
+                        filesend = open(req[1], "r")
+                    except Exception as e:
+                        print("FIle could not be opened." + str(e))
+                        flag = -1
+                    # IF the file could be opened
+                    if flag != -1:
+                        filesize = os.path.getsize('./' + req[1])   # Gets file size in bytes
+
+                        # Create a headed containing information about the file so the server knows how to process it
+                        header = "TEXT_" + req[1] + "_" + username + "_" + str(filesize) + "_;"
+                        clientSock.send(header.encode())    # Send header to server
+
+                        # This loop ensures that it will only read data from the file while the number of bytes
+                        # read is less than or equal to the file size in bytes
+                        while count <= filesize:
+                            data = filesend.read(1024).encode()     # Read data from file in 1024 byte packets
+                            if not data:    # If the data read is null break out of loop
+                                break
+                            clientSock.send(data)       # Send each packet of data to the server
+                            count += len(data)          # increase counter by the length of the data in bytes
+
+
+                # If the data is not a text file, it is a binary file. Process is same as above
+                else:
+                    flag = 1
+                    try:
+                        filesend = open(req[1], "rb")
+                    except Exception as e:
+                        print("FIle could not be opened." + str(e))
+                        flag = -1
+                    if flag != -1:
+                        filesize = os.path.getsize('./' + req[1])
+                        header = "BIN_" + req[1] + "_" + username + "_" + str(filesize) + "_;" # Header starts with BIN
+                        clientSock.send(header.encode())
+                        while count <= filesize:
+                            data = filesend.read(1024)
+                            if not data:
+                                break
+                            clientSock.send(data)
+                            count += len(data)
+                # Get message from server that the file has sent successfully to server
+                print("Sending File")
+                message = clientSock.recv(1024)
+                print(message.decode())
+
+            # IF the header for the file received from the server is text. Create a text file and read packets into file
+            if data.decode().startswith("TEXT"):
+                count = 0
+                headerList = data.decode().split("_")
+                try:
+                    clientFile = open(headerList[1], "w")
+                except Exception as e:
+                    print("Couldnt create file!" + str(e))
+                while count < int(headerList[3]):
+                    data = clientSock.recv(1024)
+                    if data.decode() == "":
+                        break
+                    clientFile.write(data.decode())
+                    clientFile.flush()
+                    count += len(data)
+
+                print("File received:")
+                print("Name of file:" + headerList[1])
+                print("From: " + headerList[2])
+                print("Content length:" + str(os.path.getsize('./' + headerList[1])))
+                clientFile.close()
+                prompt()
+            # If the header for thefile recvied from server is BIN, create approprite file and read packets into file
+            if data.decode().startswith("BIN"):
+                count = 0
+                headerList = data.decode().split("_")
+                try:
+                    clientFile = open(headerList[1], "wb")
+                except Exception as e:
+                    print("Couldnt create file!" + str(e))
+                while count < int(headerList[3]):
+                    data = clientSock.recv(1024)
+                    if not data:
+                        break
+                    clientFile.write(data)
+                    clientFile.flush()
+                    count += len(data)
+                print("File received:")
+                print("Name of file:" + headerList[1])
+                print("From: " + headerList[2])
+                print("Content length:" + str(os.path.getsize('./' + headerList[1])))
+                prompt()
+                clientFile.close()
             # This is when the message came from another user, print and prompt the user to respond
             else:
                 sys.stdout.write(data.decode())
                 prompt()
+
         # When the user needs to input a message, format the message with the @username. Send the message off the server
         else:
-            message = sys.stdin.readline()
-            messageSend = "@" + username + ": " + message
-            clientSock.send(messageSend.encode())
-            prompt()
+            message = sys.stdin.readline().rstrip("\n")
+            if message.startswith('!'):
+                clientSock.send(message.encode())
+                prompt()
+            else:
+                messageSend = "@" + username + ": " + message
+                clientSock.send(messageSend.encode())
+                prompt()
--- server.py
+++ server.py
@@ -1,17 +1,23 @@
-# CS 3357 Assignment 1 Matthew Cheverie 251098050
+# CS 3357 Assignment 3 Matthew Cheverie 251098050
 # This file runs the server for this multi client chat application. Multiple clients are able to connect to the server
 # via multiplexing from the select package.  The sys package is used to write and read data to and from the terminal
-# as well as shutting down the program. The signal package is used to handle CTRL C inputs
+# as well as shutting down the program. The signal package is used to handle CTRL C inputs. The server also keeps track
+# of followed terms for all users as well as handles all commands send from the users.
 import sys
 from socket import *
 import select
 import signal
+import string
 
 # Declare some administrative variables
 
 HOST = 'localhost'  # Constant that contains the name of the host
 socketList = []  # List for the sockets
 users = {}  # Dictionary pairing usernames to their sockets. The username is the key
+followDict = {}  # Dictionary paring usernames to lists of followed terms and users
+PUNC = string.punctuation  # Declare constant for punctuation for right stripping
+PUNC_MISSING_AT = PUNC.replace("@",
+                               "")  # Create a variable to contain all punctuation except @. Used for left stripping
 
 
 # Function that returns all the active users in the server. It works by creating a list of all the keys (usernames) in
@@ -29,7 +35,7 @@ def getActiveUsers(dict):
 def signalHandler(sig, frame):
     print("CTRL C dectected. The chat server will be shut down and all users will be notified")
     msg = "DISCONNECT CHAT/1.0"
-    sendMsg(serverSock, msg)
+    sendServerMessage(serverSock, msg)
     try:
         serverSock.close()
     except Exception as e:
@@ -37,14 +43,6 @@ def signalHandler(sig, frame):
         print(e)
     print("Server successfully closed! Exiting program")
     sys.exit(0)
-
-
-# This function will an entry from the dictionary based on the given key passed in. It will then return the dictionary
-# Parameters: d - the dictionary, key - the key used to reference the entry to be deleted
-def removeKey(d, key):
-    r = dict(d)
-    del r[key]
-    return r
 
 
 # This function will get a clients username by referencing the socket value stored.
@@ -57,10 +55,84 @@ def getKeyByValue(dict, value):
     return keyList[position]
 
 
+# This function sends a file to a user based on the terms specified in the header. It will send a give file to all users
+# in the terms or to any user who follows a word in the terms list.
+# parameters: Socket - the socket from the user who sent the file, header - the file header to be sent,
+# headerList- the list created from splitting the header, terms - the terms from the request command
+def sendBasedOnTerms(sock, header, headerList, terms):
+    termList = terms.split(" ")  # Create a list of all words in the terms
+
+    # IF the file to be sent is a text file
+    if headerList[0].strip() == "TEXT":
+        # Keep track of a list of sockets that we have already sent the file too. Ensures that file only gets sent
+        # To the requried user once
+        alreadySent = []
+
+        # This nested for loop will check every word in the terms list and see if it belongs to the followed list for
+        # every uesr. If So, send the packets to them and add their socket to the already sent list.
+        for user in users:
+            for word in termList:
+                word = word.lstrip(PUNC_MISSING_AT)
+                word = word.rstrip(PUNC)
+                if word in followDict[user] and users[user] not in alreadySent and users[user] != sock:
+                    count = 0
+                    sendText = open(headerList[1], "r")
+                    sendText.seek(0)
+                    users[user].send(header.encode())
+                    while count <= int(headerList[3]):
+                        print("Sending packets to " + user)
+                        data = sendText.read(1024).encode()
+                        if not data:
+                            break
+                        users[user].send(data)
+                        count += len(data)
+                        alreadySent.append(users[user])
+                    print("Sent text file to " + user)
+    # Same process as above excpet for binary files
+    else:
+        alreadySent = []
+        for user in users:
+            for word in termList:
+                word = word.lstrip(PUNC_MISSING_AT)
+                word = word.rstrip(PUNC)
+                if word in followDict[user] and users[user] not in alreadySent and users[user] != sock:
+                    count = 0
+                    sendFile = open(headerList[1], "rb")
+                    sendFile.seek(0)
+                    users[user].send(header.encode())
+                    while count <= int(headerList[3]):
+                        print("Sending packets to " + user)
+                        data = sendFile.read(1024)
+                        if not data:
+                            break
+                        users[user].send(data)
+                        count += len(data)
+                    alreadySent.append(users[user])
+                    print("Sent binary file to " + user)
+                    # sendFile.close()
+    successMessage = "File Sent!"
+    sock.send(successMessage.encode())
+
+
+# Function to send messages sent from other users to users who are following terms in their message. Works similar to
+# function above
+# Params: socket - the socket of the user sending the message, message - the message to be sent
+def sendMsg(sock, message):
+    wordList = message.split(' ')
+    alreadySent = []
+    for word in wordList:
+        word = word.lstrip(PUNC_MISSING_AT)
+        word = word.rstrip(PUNC)
+        for user in users:
+            if word in followDict[user] and users[user] not in alreadySent and users[user] != sock:
+                users[user].send(message.encode())
+                alreadySent.append(users[user])
+
+
 # This function will try send a message to all active sockets accept the one passed in and the server socket. If an
 # exception is thrown, this means the socket is not active. The socket will be removed from the active socket list and
 # the user will be removed from the user dictionary
-def sendMsg(sock, message):
+def sendServerMessage(sock, message):
     for socket in socketList:
         if socket != serverSock and socket != sock:
             try:
@@ -69,7 +141,8 @@ def sendMsg(sock, message):
                 socket.close()
                 socketList.remove(socket)
                 dictKey = getKeyByValue(users, socket)
-                users = removeKey(users, dictKey)
+                followDict.pop(dictKey)
+                users.pop(dictKey)
 
 
 # This function will attempt to register the user in the client dictionary. It will receive a message from the socket
@@ -85,17 +158,20 @@ def registerUser(socket):
         print(error400msg)
         socket.send(error400msg.encode())
         return False
-
+    name = registerList[1]
     # If the registration message is invalid
     if len(registerList) != 3:
-        error400msg = "400 Invalid registration for user " + registerList[1]
+        error400msg = "400 Invalid registration for user " + name
         print(error400msg)
         socket.send(error400msg.encode())
         return False
-
+    if name == 'all':
+        error400msg = "400 Invalid registration for user " + name + ". All is reserved username which cannot be taken."
+        print(error400msg)
+        socket.send(error400msg.encode())
     # If there is a client already registered with that name
-    if registerList[1] in users:
-        error401msg = "401 Client already registered for user " + registerList[1]
+    elif name in users:
+        error401msg = "401 Client already registered for user " + name
         print(error401msg)
         socket.send(error401msg.encode())
         return False
@@ -103,13 +179,140 @@ def registerUser(socket):
     # If the registration was successful, send message to user that registration was successful. Notify all other clients
     # that a new user has joined the server. Add the user to the dictionary
     else:
-        success200msg = "200 Registration successful for user " + registerList[1]
+        success200msg = "200 Registration successful for user " + name
         print(success200msg)
-        users[registerList[1]] = socket
+        userAt = '@' + name
+        users[name] = socket
+        followDict[name] = ['@all', userAt]
         socket.send(success200msg.encode())
-        sendToAllmsg = "Client @" + registerList[1] + " has joined the server! Say hi!!\n"
-        sendMsg(socket, sendToAllmsg)
+        sendToAllmsg = "Client @" + name + " has joined the server! Say hi!!\n"
+        sendServerMessage(socket, sendToAllmsg)
         return True
+
+
+# Function to get and return the size in bytes of any passed in file
+def getFileSize(file):
+    file.seek(0, 2)
+    size = file.tell()
+    return size
+
+
+# Function for handling commands from any given user.
+# Parameters: command - the command from the user, socket - the socket from which the command was sent from
+def commandHandler(command, socket):
+    commandArgs = command.split(' ')  # Create a list of command arguments
+    user = getKeyByValue(users, socket)  # get the username based on the socket
+
+    # IF the command is !list, send a comma seperated list of all active users to the lcient
+    if command.strip() == '!list':
+        theUsers = getActiveUsers(users)
+        csList = ", ".join(theUsers)
+        sendM = "The active users are: " + csList
+        socket.send(sendM.encode())
+
+    # If the command is !exit, send a disconnect exit request to the client for processing. Remove the user from the
+    # dictionary of users and dictionary of followed items. Send a message to all users displaying remaing active users.
+    # Remove the socket from list of sockets and close that socket
+    if command.strip() == '!exit':
+        exitMes = "DISCONNECT EXIT"
+        socket.send(exitMes.encode())
+        followDict.pop(user)
+        users.pop(user)
+        activeUsers = getActiveUsers(users)
+        activeUserMessage = "The remaining active users are " + str(activeUsers) + ".\n"
+        sendServerMessage(socket, activeUserMessage)
+        # Remove the socket from the list of active sockets and close that socket
+        socketList.remove(socket)
+        socket.close()
+
+    # Diplays a list of all followed items for the user
+    elif command.strip() == '!follow?':
+        userFollow = ", ".join(followDict[user])
+        message = "Your followed items are: " + userFollow
+        socket.send(message.encode())
+
+    # adds an entry to the list for the user in the follow dictionary if they are not already following that item
+    elif commandArgs[0].strip() == '!follow':
+        if commandArgs[1].strip() in followDict[user]:
+            message = "You already follow " + commandArgs[
+                1].strip() + ". Cannot follow an item that is already followed"
+            socket.send(message.encode())
+        else:
+            followDict[user].append(commandArgs[1].strip())
+            message = "You are now following " + commandArgs[1].strip()
+            socket.send(message.encode())
+
+    # Unfollows a given term if the user is following it. Cannot unfollow the all user or themselves.
+    elif commandArgs[0].strip() == '!unfollow':
+        userAt = '@' + user
+        if commandArgs[1].strip() not in followDict[user]:
+            message = "Cannot unfollow an item that you are not following!!!"
+            socket.send(message.encode())
+        elif commandArgs[1].strip() == '@all' or commandArgs[1].strip() == userAt:
+            message = "Cannot unfollow yourself or the all user!"
+            socket.send(message.encode())
+        else:
+            followDict[user].remove(commandArgs[1].strip())
+            message = "Successfully unfollowed " + commandArgs[1].strip()
+            socket.send(message.encode())
+    # IF the user wants to send a file, this command will request the file from the user. It will then store it on the
+    # Server for further processing.
+    elif commandArgs[0].strip() == '!attach':
+        terms = ' '.join(commandArgs[2:])
+        reqMessage = "REQUEST " + commandArgs[1]
+        socket.send(reqMessage.encode())
+
+        if commandArgs[1].endswith("txt"):
+            status = "HEADER_SUC"
+            count = 0
+            try:
+                header = socket.recv(1024).decode()
+            except Exception as e:
+                print(e)
+                status = "HEADER_FAIL"
+                print(status)
+            if status == "HEADER_SUC":
+                headerList = header.split("_")
+                try:
+                    serverFile = open(headerList[1], "w+")
+                except Exception as e:
+                    print("The File could not be opened. " + e)
+                while count < int(headerList[3]):
+                    data = socket.recv(1024)
+                    if data.decode() == "":
+                        break
+                    serverFile.write(data.decode())
+                    serverFile.flush()
+                    count += len(data)
+                print("File received from: " + headerList[2])
+                sendBasedOnTerms(socket, header, headerList, terms)
+                serverFile.close()
+        else:
+            status = "HEADER_SUC"
+            count = 0
+            try:
+                header = socket.recv(1024).decode()
+            except Exception as e:
+                print(e)
+                status = "HEADER_FAIL"
+                print(status)
+            if status == "HEADER_SUC":
+                headerList = header.split("_")
+                try:
+                    serverFile = open(headerList[1], "wb+")
+                except Exception as e:
+                    print("The File could not be opened. " + e)
+                while count < int(headerList[3]):
+                    data = socket.recv(1024)
+                    if not data:
+                        break
+                    serverFile.write(data)
+                    serverFile.flush()
+                    count += len(data)
+                print("File received from: " + headerList[2])
+                sendBasedOnTerms(socket, header, headerList, terms)
+                serverFile.close()
+            print("File received")
 
 
 # Where the program starts - Setting up server
@@ -140,38 +343,42 @@ while True:
         else:
             # Try to receive data from the socket
             try:
-                data = socket.recv(4096)
+                data = socket.recv(1024)
 
                 # If no data is recived, that means the user from which the socket is connected to is offline
                 if not data:
                     dictKey = getKeyByValue(users, socket)
                     leavingMessage = "Client @" + dictKey + " is offline\n"
                     print(leavingMessage)
-                    sendMsg(socket, leavingMessage)     #Send message to all users that the person is offline
-                    users = removeKey(users, dictKey)
+                    sendServerMessage(socket, leavingMessage)  # Send message to all users that the person is offline
+                    followDict.pop(dictKey)
+                    users.pop(dictKey)
                     socketList.remove(socket)
                     socket.close()
 
                 # If the data recvieed is a disconnect message, start the disconnect process
                 elif data.decode().startswith('DISCONNECT'):
-
-                    #This Block notifies users that the client has left the server
+                    # This Block notifies users that the client has left the server
                     message = data.decode()
                     splitMessage = message.split(' ')
                     userDisconnect = splitMessage[1]
                     userMessage = "Client " + userDisconnect + " has left the server."
                     print(userMessage)
-                    sendMsg(socket, userMessage)
+                    sendServerMessage(socket, userMessage)
 
                     # This removes the user from the dictionary of active users and lets everyone know who is active
-                    users = removeKey(users, userDisconnect)
+                    followDict.pop(userDisconnect)
+                    users.pop(userDisconnect)
                     activeUsers = getActiveUsers(users)
                     activeUserMessage = "The remaining active users are " + str(activeUsers) + ".\n"
-                    sendMsg(socket, activeUserMessage)
+                    sendServerMessage(socket, activeUserMessage)
 
                     # Remove the socket from the list of active sockets and close that socket
                     socketList.remove(socket)
                     socket.close()
+                elif data.decode().startswith('!'):
+                    command = data.decode()
+                    commandHandler(command, socket)
 
                 # This loop gets triggered when a user sends a normal message
                 else:
@@ -180,12 +387,14 @@ while True:
                     print('Recieved message from @' + dictKey + ': ' + message.strip())
                     sendMsg(socket, message)
             # The except will be triggered if the socket.recv() throws an excepion. Ususally happens if someone crashes
-            except:
+            except Exception as e:
+                print(e)
                 dictKey = getKeyByValue(users, socket)
                 leavingMessage = "Client " + dictKey + " has left the conversation unexpectedly\n"
                 print(leavingMessage)
-                sendMsg(socket, leavingMessage)
-                users = removeKey(users, dictKey)
+                sendServerMessage(socket, leavingMessage)
+                followDict.pop(dictKey)
+                users.pop(dictKey)
                 socketList.remove(socket)
                 socket.close()
                 continue
